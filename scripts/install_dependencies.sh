f7a310bd12eb94a5945078f9f6bf024279fa4e22
Damilare Koiki <koikidamilare@gmail.com>
2024-11-11 00:39:07 +0100

    Appspec and scripts

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
-cd /var/www/laravel-app
-chmod -R 755 node_modules
-chmod -R 755 public
-
-
 sudo apt update
 sudo apt install nginx
 sudo systemctl start codedeploy-agent
 composer install --prefer-dist --no-interaction
+nvm use 22
 npm install
 npm run build
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # Example script to start the server
 sudo systemctl start nginx
+sudo systemctl restart php-fpm
