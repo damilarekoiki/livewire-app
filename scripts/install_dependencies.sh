c44009ec75936001a2e5a1ce31c113a90f23637b
Damilare Koiki <koikidamilare@gmail.com>
2024-11-11 00:54:05 +0100

    Appspec and scripts

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -3,6 +3,6 @@ sudo apt update
 sudo apt install nginx
 sudo systemctl start codedeploy-agent
 # composer install --prefer-dist --no-interaction
-nvm use 22
-npm install
+# nvm use 22
+# npm install
 # npm run build
