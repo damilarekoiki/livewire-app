a60ab529a1c692a28fd81cdc862419dad8955ec5
Damilare Koiki <koikidamilare@gmail.com>
2024-11-11 04:01:45 +0100

    Appspec and scripts

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -7,7 +7,10 @@ sudo systemctl start codedeploy-agent
 
 composer install --prefer-dist --no-interaction
 
-export PATH=$PATH:/usr/local/bin  # Adjust this path if npm is located elsewhere
+#add npm and node to path
+export NVM_DIR="$HOME/.nvm" 
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm 
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
 npm install
 npm run build
