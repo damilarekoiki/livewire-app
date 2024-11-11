b11ac4feb488e58b51aaf7d80f57457fc07d6e3d
Damilare Koiki <koikidamilare@gmail.com>
2024-11-11 04:46:42 +0100

    Appspec and scripts

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -7,19 +7,19 @@ sudo apt update
 sudo apt install nginx
 sudo systemctl start codedeploy-agent
 
-composer install --prefer-dist --no-interaction
+# composer install --prefer-dist --no-interaction
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
+# export NVM_DIR="$HOME/.nvm"
+# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-nvm install 22
+# nvm install 22
 
-npm install
+# npm install
 
-chmod -R 755 /var/www/laravel-app
-chmod -R 755 /var/www/laravel-app/node_modules
-chmod -R 755 /var/www/laravel-app/public
+# chmod -R 755 /var/www/laravel-app
+# chmod -R 755 /var/www/laravel-app/node_modules
+# chmod -R 755 /var/www/laravel-app/public
 
-npm run build
+# npm run build
