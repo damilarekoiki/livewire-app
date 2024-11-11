1b1e38038ba7a0b230348430181661a41f341c5b
Damilare Koiki <koikidamilare@gmail.com>
2024-11-11 06:57:43 +0100

    Appspec and scripts

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-cd /var/www/laravel-app
-
 sudo apt update
 sudo apt install nginx
 sudo systemctl start codedeploy-agent
--- scripts/setup_db.sh
+++ scripts/setup_db.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+cd /var/www/laravel-app
+
 if [ ! -f database/database.sqlite ]; then
   touch database/database.sqlite
 fi
