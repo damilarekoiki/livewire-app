10d2c14afcbf3842f7f6ac91ed71c0043d35b101
Damilare Koiki <koikidamilare@gmail.com>
2024-11-11 03:48:44 +0100

    Appspec and scripts

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 cd /var/www/laravel-app
+
 sudo apt update
 sudo apt install nginx
 sudo systemctl start codedeploy-agent
 
 composer install --prefer-dist --no-interaction
+
+source ~/.nvm/nvm.sh
+
+nvm use 22
 
 npm install
 npm run build
