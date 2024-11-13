07b2aea4335543cdddc528dbc5351916e08ab961
Damilare Koiki <koikidamilare@gmail.com>
2024-11-13 19:00:31 +0100

    Add permissions to sqlite db in appspec

--- scripts/setup_db.sh
+++ scripts/setup_db.sh
@@ -4,4 +4,10 @@ cd /var/www/laravel-app
 if [ ! -f database/database.sqlite ]; then
   touch database/database.sqlite
 fi
+
+sudo chmod 664 /database/database.sqlite
+sudo chmod 775 /database/database.sqlite
+sudo chown www-data:www-data /database/database.sqlite
+
+
 php artisan migrate --force
