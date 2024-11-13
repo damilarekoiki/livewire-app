5ec8dda0278eebafc9618cb75bd0056034c9f05c
Damilare Koiki <koikidamilare@gmail.com>
2024-11-13 19:07:51 +0100

    Add permissions to sqlite db in appspec

--- scripts/setup_db.sh
+++ scripts/setup_db.sh
@@ -5,9 +5,9 @@ if [ ! -f database/database.sqlite ]; then
   touch database/database.sqlite
 fi
 
-sudo chmod 664 /database/database.sqlite
-sudo chmod 775 /database/database.sqlite
-sudo chown www-data:www-data /database/database.sqlite
+sudo chmod 664 database/database.sqlite
+sudo chmod 775 database/database.sqlite
+sudo chown www-data:www-data database/database.sqlite
 
 
 php artisan migrate --force
