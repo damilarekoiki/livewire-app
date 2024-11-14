214f49b998e1de7a7bb0b5c6d77ef1319b56bd16
Damilare Koiki <koikidamilare@gmail.com>
2024-11-14 10:36:07 +0100

    Add permissions in bash script

--- scripts/setup_db.sh
+++ scripts/setup_db.sh
@@ -9,5 +9,14 @@ sudo chmod 664 database/database.sqlite
 sudo chmod 775 database/database.sqlite
 sudo chown www-data:www-data database/database.sqlite
 
+sudo chmod -R 775 /var/www/laravel-app/storage /var/www/laravel-app/bootstrap/cache
+sudo chown -R www-data:www-data /var/www/laravel-app/storage /var/www/laravel-app/bootstrap/cache
+
+php artisan cache:clear
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+
+
 
 php artisan migrate --force
